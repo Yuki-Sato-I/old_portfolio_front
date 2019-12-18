e903787be69055473ac97067e675b7ff53ee5da7
YukiSato <yukiyukiamam@gmail.com>
2019-12-18 23:11:55 +0900

    [add]loading機能の追加
    トップページでloadingを挟むようにしました.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,15 @@
-import React, { useState, useCallback, useEffect } from "react";
-import axios from "axios";
-import ReactFullpage from '@fullpage/react-fullpage';
+import React, { Suspense } from "react";
 
 // my component
 import './App.css';
-import { Top } from './components/pages/top/top'
-import { NavBar } from './components/common/navbar'
+
+import { Content } from './Content'
 
 export const App = () =>  {
-
-  const [userData, setUserData] = useState([]);
-  const [skillsData, setSkillsData] = useState();
-  const [topWorksData, setTopWorksData] = useState([]);
-
-  const [anchors, setAnchors] = useState(['top', 'works', 'allWorks', 'profile', 'contact']);
-
-  const fetchData = useCallback( async() => {
-
-    let user = await axios.get('/api/v1/users/1')
-      .then(response => response.data)
-      .catch(error => console.log(error));
-
-    let skills = await axios.get('/api/v1/skills')
-      .then(response => response.data)
-      .catch(error => console.log(error));
-
-    let topWorks = await axios.get('/api/v1/works/top')
-      .then(response => response.data)
-      .catch(error => console.log(error));
-
-      setTopWorksData(topWorks);
-      setSkillsData(skills);
-      setUserData(user);
-
-    topWorks.map((work, index) => {
-      setAnchors(anchors.splice(2+index, 0, `work${index+1}`));
-    })
-    console.log(anchors);
-  },[]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-
   return (
-    <div className="container">
-      <NavBar works={topWorksData}/>
-      <ReactFullpage
-        //fullpage options
-        licenseKey = {'YOUR_KEY_HERE'}
-        resize={true}
-        scrollingSpeed = {1000} /* Options here */
-        anchors= { anchors }
-        render={({ state, fullpageApi }) => {
-          return (
-            <ReactFullpage.Wrapper>
-              <Top user={userData}
-                   skills={skillsData}/>
-              <div className="section">
-                <p>Section 2</p>
-              </div>
-              <div className="section">
-                <p>Section 3</p>
-              </div>
-              <div className="section">
-                <p>Section 4</p>
-                {/* <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button> */}
-              </div>
-
-            </ReactFullpage.Wrapper>
-          );
-        }}
-      />
-    </div>
+    <Suspense fallback={<div>loading......</div>}>
+      <Content />
+    </Suspense>
   );
 }
 
---
+++ src/Content.js
@@ -0,0 +1,84 @@
+import React, {useState} from "react";
+import axios from "axios";
+import ReactFullpage from '@fullpage/react-fullpage';
+
+// my component
+import './App.css';
+import { Top } from './components/pages/top/top'
+import { NavBar } from './components/common/navbar'
+
+let cache;
+
+export const Content = () => {
+  const [anchors, setAnchors] = useState(['top', 'works', 'allWorks', 'profile', 'contact']);
+  if(cache){
+    console.log(cache);
+    return(
+      <div className="container">
+        <NavBar works={cache.topWorks}/>
+        <ReactFullpage
+          //fullpage options
+          licenseKey = {'YOUR_KEY_HERE'}
+          resize={true}
+          scrollingSpeed = {1000} /* Options here */
+          anchors= { cache.anchors }
+          render={({ state, fullpageApi }) => {
+            return (
+              <ReactFullpage.Wrapper>
+                <Top user={cache.user}
+                    skills={cache.skills}/>
+                <div className="section">
+                  <p>Section 2</p>
+                </div>
+                <div className="section">
+                  <p>Section 3</p>
+                </div>
+                <div className="section">
+                  <p>Section 4</p>
+                  {/* <button onClick={() => fullpageApi.moveSectionDown()}>
+                    Click me to move down
+                  </button> */}
+                </div>
+                <div className="section">
+                  <p>Section 5</p>
+                </div>
+                <div className="section">
+                  <p>Section 6</p>
+                </div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        />
+      </div>
+    );
+  }
+
+  throw (async() => {
+    console.log(anchors);
+    let user = await axios.get('/api/v1/users/1')
+      .then(response => response.data)
+      .catch(error => console.log(error));
+
+    let skills = await axios.get('/api/v1/skills')
+      .then(response => response.data)
+      .catch(error => console.log(error));
+
+    let topWorks = await axios.get('/api/v1/works/top')
+      .then(response => {
+        response.data.map((work, index) => {
+          setAnchors(anchors.splice(2+index, 0, `work${index+1}`));
+        })
+        return response.data
+      })
+      .catch(error => console.log(error));
+    console.log(anchors);
+    cache = {
+      user: user,
+      skills: skills,
+      topWorks: topWorks,
+      anchors: anchors
+    };
+  })();
+}
+
+export default Content;
