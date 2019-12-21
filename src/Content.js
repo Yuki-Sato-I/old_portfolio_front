433858e13240ea8b23312e554524e6f3dec2a920
YukiSato <yukiyukiamam@gmail.com>
2019-12-21 20:55:02 +0900

    [add]loading画面のanimetion追加
    loadingのアニメーションを追加しました.

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
 import ReactFullpage from '@fullpage/react-fullpage';
+import ReactAnime from 'react-animejs'
 
 // my component
 import './App.css';
@@ -9,7 +10,7 @@ import { NavBar } from './components/common/navbar'
 import { Work } from "./components/pages/work/Work";
 import { Profile } from "./components/pages/profile/Profile";
 import { Contact } from "./components/pages/contact/Contact";
-
+const {Anime} = ReactAnime
 let cache;
 
 export const Content = () => {
@@ -17,9 +18,15 @@ export const Content = () => {
   const onLeave = (origin, destination, direction) => {
     console.log("Leaving section " + origin.index);
   }
+
   const afterLoad = (origin, destination, direction) => {
     console.log("After load: " + destination.index);
   }
+
+  function sleepByPromise(sec) {
+    return new Promise(resolve => setTimeout(resolve, sec));
+  }
+
   if(cache){
     return(
       <div className="container">
@@ -62,6 +69,7 @@ export const Content = () => {
   }
 
   throw (async() => {
+    const startTime = performance.now();
     let user = await axios.get('/api/v1/users/1')
       .then(response => response.data)
       .catch(error => console.log(error));
@@ -84,13 +92,122 @@ export const Content = () => {
       topWorks: topWorks,
       anchors: anchors
     };
+    const endTime = performance.now();
+    if(endTime - startTime < 2800.0){
+      await sleepByPromise(2800 - endTime + startTime);
+    }
   })();
 }
 export const ContentFallback = () => {
   return (
-    <div>
-      loading...
-    </div>
+    <Anime
+      initial={[
+        {
+          targets: '.ml5 .line',
+          opacity: [0.5, 1],
+          scaleX: [0, 1],
+          easing: "easeInOutExpo",
+          duration: 300
+        },
+        {
+          targets: '.ml5 .line',
+          duration: 300,
+          easing: "easeOutExpo",
+          translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+        },
+        {
+          targets: '.ml5 .ampersand',
+          opacity: [0, 1],
+          scaleY: [0.5, 1],
+          easing: "easeOutExpo",
+          duration: 300,
+          offset: '-=600'
+        },
+        {
+          targets: '.ml5 .letters-left',
+          opacity: [0, 1],
+          translateX: ["0.5em", 0],
+          easing: "easeOutExpo",
+          duration: 400,
+          offset: '-=300'
+        },
+        {
+          targets: '.ml5 .letters-right',
+          opacity: [0, 1],
+          translateX: ["-0.5em", 0],
+          easing: "easeOutExpo",
+          duration: 400,
+          offset: '-=600'
+        },
+        {
+          targets: '.ml5',
+          opacity: 0,
+          duration: 800,
+          easing: "easeOutExpo",
+          delay: 500
+        },
+        {
+          loop: true
+        }
+      ]}
+    >
+      <h1 className="ml5">
+        <span className="text-wrapper">
+          <span className="line line1"></span>
+          <span className="letters letters-left">Now</span>
+          <span className="letters ampersand">&amp;</span>
+          <span className="letters letters-right">Loading</span>
+          <span className="line line2"></span>
+        </span>
+      </h1>
+      <style>
+        {`
+          .ml5 {
+            text-align: center;
+            line-height: 90vh;
+            position: relative;
+            font-weight: 300;
+            font-size: 4.5em;
+            color: white;
+          }
+          
+          .ml5 .text-wrapper {
+            position: relative;
+            display: inline-block;
+            padding-top: 0.1em;
+            padding-right: 0.05em;
+            padding-bottom: 0.15em;
+            line-height: 1em;
+          }
+          
+          .ml5 .line {
+            position: absolute;
+            left: 0;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+            height: 3px;
+            width: 100%;
+            background-color: white;
+            transform-origin: 0.5 0;
+          }
+          
+          .ml5 .ampersand {
+            font-family: Baskerville, "EB Garamond", serif;
+            font-style: italic;
+            font-weight: 400;
+            width: 1em;
+            margin-right: -0.1em;
+            margin-left: -0.1em;
+          }
+          
+          .ml5 .letters {
+            display: inline-block;
+            opacity: 0;
+          }
+        `}
+      </style>
+    </Anime>
   );
 }
 
